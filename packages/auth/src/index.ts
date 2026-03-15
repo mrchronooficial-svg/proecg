646d13cacd288d4912d01523eb8d968e51b8a5f6
Rafael <rlacoste@clickcannabis.com>
2026-03-15 16:26:02 -0300

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import prisma from "@proecg/db";
+import { env } from "@proecg/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  plugins: [nextCookies()],
+});
